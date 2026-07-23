f7b3309c5edc75af91f0a2e641906a63a98c4aca
MejriZiad <mejri.ziad@gmail.com>
2026-07-23 11:54:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'revisions-tunisie-v4';
+const CACHE_NAME = 'revisions-tunisie-v5';
 const urlsToCache = [
   '/',
   '/index.html',
   '/revision.html',
   '/ED-French-ini.js',
+  '/ED-French-header.html',
+  '/ED-French-footer.html',
   '/manifest.json',
   '/icon-96x96.png',
   '/semaine1-classes-grammaticales.html',
@@ -18,12 +20,32 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache).catch(() => {}))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(cached => {
+      const fetchPromise = fetch(event.request).then(response => {
+        // Only cache successful responses that are not HTML error pages
+        if (response.ok && response.headers.get('Content-Type')?.includes('javascript')) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
   );
 });
