60d8fb099b63d2ab65e6dd7c720dbbaea3b580d9
MejriZiad <mejri.ziad@gmail.com>
2026-07-24 12:46:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – Révisions Tunisie
-const CACHE_NAME = 'revisions-tunisie-v1.1.8';
+const CACHE_NAME = 'revisions-tunisie-v1.1.9';
 const urlsToCache = [
   '/',
   '/index.html',
