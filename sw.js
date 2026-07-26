4cd677df042c1af3c3049162020a7f060332bf80
MejriZiad <mejri.ziad@gmail.com>
2026-07-26 12:52:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – Educational Dashboard – French
-const CACHE_NAME = 'revisions-tunisie-v1.3.1';
+const CACHE_NAME = 'revisions-tunisie-v1.3.2';
 const urlsToCache = [
     '/',
     '/index.html',
