017cb59b49eecb487ec77f1d1c066db76f7f04db
MejriZiad <mejri.ziad@gmail.com>
2026-07-26 18:35:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – Educational Dashboard – French
-const CACHE_NAME = 'revisions-tunisie-v1.3.6'; // bump version to force cache refresh
+const CACHE_NAME = 'revisions-tunisie-v1.3.7'; // bump version to force cache refresh
 const urlsToCache = [
     '/',
     '/index.html',
