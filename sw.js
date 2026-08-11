ca97988e305dbc66cf4e889b5ebc0dfa97fa6673
MejriZiad <mejri.ziad@gmail.com>
2026-08-12 00:11:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('db-utils.js');
 // Service Worker – Educational Dashboard – French
-const CACHE_NAME = 'ed-french-v2.0.5'; // bump version to force cache refresh
+const CACHE_NAME = 'ed-french-v2.0.6'; // bump version to force cache refresh
 const urlsToCache = [
     '/',
     '/index.html',
